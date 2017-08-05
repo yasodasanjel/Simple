a1da598c67d22775bc5704b19b9a33c1b2bb4225
Yasoda Sanjel <yasodasanjel@gmail.com>
2017-08-05 22:27:12 +0545

    final

--- boolean.py
+++ boolean.py
@@ -7,6 +7,7 @@ print(False or True)
 print(True or 1==1)
 print(1!=2)
 print("These are the examples of boolean")
+print("should work more")
 
 #
 ###### and operator = both comparisons have to be True in order for the whole command to be True
